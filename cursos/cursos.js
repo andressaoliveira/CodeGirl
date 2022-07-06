84303a6d8669dfde7ddc58b3d6f5d83041cd087c
Andressa Oliveira <andressa.pco@hotmail.com>
2022-07-05 21:27:20 -0300

    nova tela cursos

---
+++ cursos/cursos.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('cadastro');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    $('#alerta-cadastrado-sucesso').hide();
+    $('#alerta-usuario-existente').hide();
+
+    const contato = JSON.stringify({
+        nome: document.getElementById('inputNome').value,
+        sobreNome: document.getElementById('inputSobrenome').value,
+        email: document.getElementById('inputEmail').value,
+        receberNovidades: document.getElementById('checkNovidades').checked
+    });
+
+    var requestURL = 'https://codegirl.azurewebsites.net/Interessados';
+    var request = new XMLHttpRequest();
+
+    request.open("POST", requestURL);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(contato);
+
+    request.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 204) {
+            console.log('teste2');
+            $('#alerta-cadastrado-sucesso').show();
+        }
+        if (this.status === 500) {
+            $('#alerta-usuario-existente').show();
+        }
+    }
+});
