4fdb8c014ac2b973648a50c73c83fff47bfbdf74
Andressa Oliveira <andressa.pco@hotmail.com>
2024-02-16 17:21:49 -0300

    att

---
+++ script.js
@@ -0,0 +1,10 @@
+function getAltura(pagina) {
+    console.log("pagina:", pagina)
+    let elemento = pagina + "-iframe"
+    const iframe = document.getElementById(elemento);
+    console.log("iframe:", iframe)
+
+    var altura = iframe.contentWindow.document.getElementById(pagina).scrollHeight;
+    console.log("altura " + pagina, altura)
+    iframe.height = altura;
+}
